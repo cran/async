c9de2fa897c2669c4928f81ec0b406ec5eebf05e
Peter Meilstrup <peter.meilstrup@gmail.com>
2023-02-20 23:00:02 +0000

    version 0.3

--- inst/doc/clapping.R
+++ inst/doc/clapping.R
@@ -14,28 +14,38 @@ print_pattern <- function(counts = c(3, 2, 1, 2), repeats=4) {
 print_pattern(repeats=4);
 
 ## -----------------------------------------------------------------------------
-library(async) # for gen
-gen_pattern <- function(counts = c(3, 2, 1, 2)) { force(counts)
-  gen({
-    repeat {
-      for (n in counts) {
-        for (j in 1:n)
-          yield(1)
-        yield(0)
-      }
+library(async)
+counts <- c(3, 2, 1, 2)
+pattern <- gen({
+  repeat {
+    for (n in counts) {
+      for (j in 1:n)
+        yield(1)
+      yield(0)
     }
-  })
+  }
+})
+
+## -----------------------------------------------------------------------------
+for (i in 1:24) {
+  cat(nextOr(pattern, break))
 }
+cat("\n")
 
 ## -----------------------------------------------------------------------------
-library(iterators)
-p <- gen_pattern()
-for (i in 1:23) { cat(nextElem(p)) }; cat("\n")
+gen_pattern <- gen(function(counts = c(3, 2, 1, 2)) {
+  repeat {
+    for (n in counts) {
+      for (j in 1:n)
+        yield(1)
+      yield(0)
+    }
+  }
+})
 
 ## -----------------------------------------------------------------------------
-library(magrittr)
 show_head <- function(x, n=24) {
-  x %>% itertools::ilimit(n) %>% as.list() %>% deparse() %>% cat(sep="\n")
+  x |> ilimit(n) |> as.list() |> deparse() |> cat(sep="\n")
 }
 show_head(gen_pattern(), 24)
 
@@ -53,18 +63,58 @@ show_head(gen_pattern(), 24)
 #  play(claps[[2]])
 
 ## ---- eval=FALSE--------------------------------------------------------------
-#  gen_pattern() %>% itertools::ilimit(36) %>% iplay(claps, 360)
+#  gen_pattern() |> ilimit(36) |> iplay(claps, 360)
 
 ## -----------------------------------------------------------------------------
-drop_one_after <- function(g, n, sep=character(0)) {  list(g, n, sep)
-  gen(
-    repeat {
-      for (i in 1:n) yield(nextElem(g))
-      nextElem(g) #drop
-      cat(sep) # print a seperator after every skip
-    }
-  )
+iapply <- function(it, f, ...) { list(it, f, ...)
+  iteror(function(or) {
+    f(nextOr(it, return(or)), ...)
+  })
 }
+
+isink <- function(it, then=invisible(NULL)) {
+  repeat nextOr(it, break)
+  then
+}
+
+## -----------------------------------------------------------------------------
+g <- gen_pattern() |> ilimit(24) |> iapply(cat) |> isink(cat("\n"))
+
+## -----------------------------------------------------------------------------
+gapply <- gen(function(it, f, ...) for (x in it) yield(f(x, ...)) )
+gsink <- function(it, then=NULL) {run(for (i in it) NULL); then}
+
+## -----------------------------------------------------------------------------
+drop_one_after <- gen(function(g, n, sep=character(0)) {
+  repeat {
+    for (i in 1:n) yield(nextOr(g, break))
+    nextOr(g, break) #drop
+    cat(sep) # print a seperator after every skip
+  }
+})
+
+## -----------------------------------------------------------------------------
+iseq() |>
+ilimit(12) |>
+drop_one_after(3, "\n") |>
+iapply(cat, "") |>
+isink()
+
+## -----------------------------------------------------------------------------
+clapping_music <- function(n=12, counts=c(3,2,1,2), sep=" ") {
+  cell <- sum(counts+1) # how long?
+  a <- gen_pattern(counts)
+  b <- gen_pattern(counts) |> drop_one_after(n*cell, sep)
+  # add them together and limit the output
+  gen(for (i in 1:(n*(cell+1)*cell)) {
+    x <- nextOr(a, break)
+    y <- nextOr(b, break)
+    yield(x+y)
+  })
+}
+
+## -----------------------------------------------------------------------------
+clapping_music(4, sep="\n") |> iapply(cat) |> isink(cat("\n"))
 
 ## ---- eval=FALSE--------------------------------------------------------------
 #  iplay(clapping_music(n=4, sep="\n"), claps, 480)
---
+++ inst/doc/language.R
@@ -0,0 +1,121 @@
+## -----------------------------------------------------------------------------
+library(async)
+pausables() |> vapply(as.character, "") |> sort()
+
+## ---- error=TRUE--------------------------------------------------------------
+L <- run( list( if (x) yield(y) else yield(x) ) )
+
+## -----------------------------------------------------------------------------
+# given a sequence of sequences, concatenate them like c() into one sequence
+chain <- function(sequences) {
+  force(sequences)
+  gen({
+    for (seq in sequences) {
+      yieldFrom(seq)
+    }
+  })
+}
+
+## -----------------------------------------------------------------------------
+chain <- gen(function(sequences) {
+  for(seq in sequences) {
+    yieldFrom(seq)
+  }
+})
+
+## ---- error=TRUE--------------------------------------------------------------
+names <- c("Ana", "Bob", "Charlie")
+g <- gen({
+  greet <- function(name) yield(paste0("Good morning, ", name))
+  for (i in names) {
+    greet(name)
+  }
+})
+as.list(g)
+
+## -----------------------------------------------------------------------------
+names <- c("Ana", "Bob", "Charlie")
+greet <- gen(function(name) {
+  yield(paste0("Good morning, ", name))
+})
+run(type="", {
+  for (n in names) {
+    yieldFrom(greet(n))
+  }
+})
+
+## ---- echo=FALSE--------------------------------------------------------------
+download_file <- function(url) async:::mock_promise()
+
+## -----------------------------------------------------------------------------
+file_dataset <- async({
+  filename <- await( download_file(url) )
+  switch(getExtension(filename),
+      "txt"=readTxt(filename),
+      "csv"=tryCatch(readCSV(filename), error=goto("txt")),
+      "json"={
+          if (!await(validateSchema(filename))) goto("txt")
+          tryCatch(read_json(filename), error=goto("txt"))
+      },
+      "zip"= {
+          unzipped <- unzip(filename)
+          on.exit(unlink(unzipped))
+          unzip(filename)
+          goto() # i.e. run getExtension on the new filename
+      }
+  )
+})
+
+## ----eval=FALSE---------------------------------------------------------------
+#  graphAsync(file_dataset, type="svg")
+
+## ---- echo=FALSE--------------------------------------------------------------
+with_timeout <- function(...)async:::mock_promise()
+
+## -----------------------------------------------------------------------------
+  as <- async({
+    tryCatch(
+      await(with_timeout(x, 10)),
+      error=return("Value was not available"))
+  })
+
+## -----------------------------------------------------------------------------
+as <- async({
+  await(with_timeout(promise()),
+        "Value was not available")
+})
+
+## -----------------------------------------------------------------------------
+fileLines <- gen(function(...) {
+  f <- file(..., open="rt")
+  on.exit(close(f))
+  while (length(line <- readLines(f, 1)) > 0) yield(line)
+})
+length(as.list(fileLines("language.Rmd")))
+
+## ---- error=TRUE--------------------------------------------------------------
+x <- run({
+    on.exit({return("this instead")})
+    stop("an error")
+})
+x
+
+## -----------------------------------------------------------------------------
+finished <- FALSE
+g <- gen({
+  on.exit(finished <<- TRUE)
+  for (i in 1:10) yield(i)
+})
+for (j in 1:10) nextOr(g, break)
+finished
+nextOr(g, "stop")
+finished
+
+## -----------------------------------------------------------------------------
+bench <- microbenchmark::microbenchmark(
+  as.list(gen(for (i in iterators::iter(1:1000)) yield(i))),
+  as.list(gen(for (i in iteror(1:1000)) yield(i))),
+  times=10
+)
+summary(bench)[, c("expr", "median")]
+
